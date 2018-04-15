607c1c79e2d9b1ad4c89a6371d1c7e0349a25bec
fiszfisz <wojciech.maliga@gmail.com>
2018-04-15 19:13:17 +0200

    Implemented simple cache

--- app/src/main/java/com/wojtek/biblioteczka/ImageTools.java
+++ app/src/main/java/com/wojtek/biblioteczka/ImageTools.java
@@ -5,16 +5,27 @@ import android.graphics.BitmapFactory;
 
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.WeakHashMap;
 
 public class ImageTools {
 
+    static WeakHashMap<String, Bitmap> bitmapCache = new WeakHashMap<>();
+
     static Bitmap downloadBitmap(String address) {
         Bitmap bmp;
+
+        synchronized (bitmapCache) {
+            if (bitmapCache.containsKey(address)) {
+                bmp = bitmapCache.get(address);
+                return bmp;
+            }
+        }
 
         try {
             URL url = new URL(address);
             URLConnection connection = url.openConnection();
             bmp = BitmapFactory.decodeStream(connection.getInputStream());
+            bitmapCache.put(address, bmp);
         } catch(Exception e) {
             bmp = null;
         }
