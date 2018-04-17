9be4bce4c94c3f7e8f363f0b98bd4bfc744d72b1
fiszfisz <wojciech.maliga@gmail.com>
2018-04-17 18:52:33 +0200

    Parsing data from external websites added

--- app/src/main/java/com/wojtek/biblioteczka/EditBookActivity.java
+++ app/src/main/java/com/wojtek/biblioteczka/EditBookActivity.java
@@ -1,18 +1,133 @@
 package com.wojtek.biblioteczka;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.ref.WeakReference;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EditBookActivity extends AppCompatActivity {
 
     static final String tag = "EditBookActivity";
 
     private Book book;
+
+    private class WebsiteDownloaderTask extends AsyncTask<String, Void, String> {
+        private final String address;
+        private final WeakReference<View> viewWeakReference;
+
+        public WebsiteDownloaderTask(View view, String url) {
+            address = url ;
+            viewWeakReference = new WeakReference<>(view);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // Placeholder
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String data;
+
+            try {
+                URL url = new URL(address);
+                URLConnection connection = url.openConnection();
+                InputStream is = connection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+                String line;
+                StringBuffer buffer = new StringBuffer();
+
+                while ((line = br.readLine()) != null) {
+                    buffer.append(line);
+                    buffer.append('\n');
+                }
+
+                br.close();
+                is.close();
+
+                data = buffer.toString();
+            } catch(Exception e) {
+                data = null;
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            if (isCancelled()) {
+                data = null;
+            }
+
+            if (viewWeakReference != null) {
+                View view = viewWeakReference.get();
+                if(view != null) {
+                    if (data != null) {
+                        String line;
+                        BufferedReader br;
+
+                        String titleText;
+                        String coverText;
+
+                        Pattern titlePattern;
+                        Pattern coverPattern;
+
+                        Matcher titleMatcher;
+                        Matcher coverMatcher;
+
+                        EditText titleEditText = view.findViewById(R.id.titleEditText);
+                        EditText coverEditText = view.findViewById(R.id.coverEditText);
+
+                        if (address.contains("empik.com")) {
+                            titlePattern = Pattern.compile("<meta property=\\\"og:title\\\" content=\\\"(.*?)\\\" \\/>");
+                            coverPattern = Pattern.compile("<meta property=\\\"og:image\\\" content=\\\"(.*?)\\\" \\/>");
+
+                            br = new BufferedReader(new StringReader(data));
+
+                            try {
+                                while ((line = br.readLine()) != null) {
+                                    titleMatcher = titlePattern.matcher(line);
+                                    if (titleMatcher.matches()) {
+                                        titleText = titleMatcher.group(1);
+                                        titleEditText.setText(titleText);
+                                        continue;
+                                    }
+
+                                    coverMatcher = coverPattern.matcher(line);
+                                    if (coverMatcher.matches()) {
+                                        coverText = coverMatcher.group(1);
+                                        coverEditText.setText(coverText);
+                                        continue;
+                                    }
+                                }
+                            } catch (Exception e) {
+                                Log.e(tag, "Unknown error while reading parsed data: " + e.getMessage());
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +145,22 @@ public class EditBookActivity extends AppCompatActivity {
         if (book != null) {
             setContents(book);
         }
+
+        if (book.isEmpty()) {
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData.Item item = clipboardManager.getPrimaryClip().getItemAt(0);
+            String pasteData = item.getText().toString();
+            if (pasteData != null) {
+                if (pasteData.startsWith("http") || pasteData.contains("www.")) {
+                    loadClipboardContents(pasteData);
+                }
+            }
+        }
+    }
+
+    private void loadClipboardContents(String address) {
+        WebsiteDownloaderTask task = new WebsiteDownloaderTask(findViewById(R.id.constraint_layout), address);
+        task.execute();
     }
 
     private void setContents(Book book) {
